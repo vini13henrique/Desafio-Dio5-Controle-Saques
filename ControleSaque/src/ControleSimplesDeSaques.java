8d0ac60921cbc0ecfa08d36b96e9cc8e06ee70be
vinicius henrique <viniciuslimalopes13@gmail.com>
2024-05-23 18:51:18 -0300

    Projeto Concluido

---
+++ ControleSaque/src/ControleSimplesDeSaques.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        double limiteDiario = scanner.nextDouble();
+	        double saque  = 0;
+	        // TODO: Crie um loop 'for' para iterar sobre os saques:
+	        for (double valorSaque  = saque; limiteDiario >valorSaque ;limiteDiario-=valorSaque){
+	            // TODO: Solicite ao usuário o valor do saque:
+	            saque = scanner.nextDouble();
+
+	            // TODO: Verifique se o valor do saque é zero, encerrando as transações:
+	            // Dica: Utilize um 'if/else' para verificar as condições do valorSaque e o limiteDiario;
+	            if (saque == 0) {
+	                System.out.println("Transacoes encerradas.");
+	                return;
+	            }else {
+
+	                // TODO: Se o valor do saque não ultrapassar o limite diário, subtraia o valor do saque do limite diário:
+	                if (saque <= limiteDiario) {
+	                    limiteDiario -= saque;
+	                    System.out.println("Saque realizado. Limite restante: " + String.format("%.1f", limiteDiario));
+	                } else {
+
+	                    // TODO: Informe que o saque foi realizado e mostre o limite restante:
+	                    System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+	                    return;
+	                }
+	            }
+	        }
+
+	        // Fechamos o Scanner para evitar vazamento de recursos:
+	        scanner.close();
+	    }
+}
